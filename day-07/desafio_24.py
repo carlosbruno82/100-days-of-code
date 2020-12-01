1270834746ea1a052985a2b331a3ca0215ee55cf
Carlos <carlao.linux@gmail.com>
2020-11-30 22:05:23 -0300

    day-07

--- day-07/desafio_24.py
+++ day-07/desafio_24.py
@@ -1,9 +1,6 @@
 cidade = str(input('Digite o nome da cidade: ').strip())
 cidade_caps = cidade.upper()
 cidade_lista = cidade_caps.split()
-santo = cidade_lista[0].find('SANTO')
+santo = 'SANTO' in cidade_lista[0]
 print("""O nome da cidade é: {}
-Primeiro nome é Santo? {}
-Se o resultado for 0 é sim
-Se o resultado for -1 é não 
-""".format(cidade, santo))
+Primeiro nome é Santo? {}""".format(cidade, santo))
